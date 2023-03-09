ce669bcdcb16a44e0336028d5e9810f3ca94481b
yingtu35 <91551415+yingtu35@users.noreply.github.com>
2023-03-09 09:24:20 +0800

    refactor code in exercises 2.20

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -3,18 +3,13 @@ import Country from "./components/Country";
 import Countries from "./components/Countries";
 import axios from 'axios'
 
-const QueryFilter = ({query, onQueryChange}) => <p>find countries <input value={query} onChange={onQueryChange}/></p>;
+const QueryFilter = ({query, setQuery}) => <p>find countries <input value={query} onChange={(e) => setQuery(e.target.value)}/></p>;
 
 function App() {
   const [countries, setCountries] = useState([]);
-  const [countryToShow, setCountry] = useState(null);
   const [query, setQuery] = useState('');
   const countriesToShow = countries.filter(country => country.name.common.toLowerCase().includes(query.toLowerCase()));
 
-  const onQueryChange = (e) => {
-    setQuery(e.target.value);
-    setCountry(null);
-  }
   useEffect(() => {
     axios.get("http://localhost:3001/countries")
     .then(response => {
@@ -24,14 +19,12 @@ function App() {
   }, [])
   return (
     <div className="App">
-      <QueryFilter query={query} onQueryChange={onQueryChange} />
-      {countryToShow
-      ? (<Country country={countryToShow} />)
-      : countriesToShow.length == 1
+      <QueryFilter query={query} setQuery={setQuery} />
+      {countriesToShow.length == 1
       ? (<Country country={countriesToShow[0]} />)
       : countriesToShow.length > 10
       ? <p>Too many matches, specify another filter</p>
-      : (<Countries countries={countriesToShow} handleShowClick={setCountry}/>)}
+      : (<Countries countries={countriesToShow} handleShowClick={setQuery}/>)}
     </div>
   );
 }
--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,7 +1,7 @@
 const Countries = ({countries, handleShowClick}) => {
     return (
       <>
-        {countries.map((country, index) => <p key={index}>{country.name.common} <button onClick={() => handleShowClick(country)}>show</button></p>)}
+        {countries.map((country, index) => <p key={index}>{country.name.common} <button onClick={() => handleShowClick(country.name.common)}>show</button></p>)}
       </>
     )
 }
